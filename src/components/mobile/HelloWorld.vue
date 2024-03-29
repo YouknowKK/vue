2a438be9df7dae3d2c718ed46b5fc1163bbad041
YouknowKK <39330481+YouknowKK@users.noreply.github.com>
2024-03-29 18:24:48 +0800

    131

--- src/components/mobile/HelloWorld.vue
+++ src/components/mobile/HelloWorld.vue
@@ -1,14 +1,36 @@
 <template>
   <div class="phone">
-    <div><img :src="banner" alt="p1" /></div>
-    <div>2</div>
-    <div>3</div>
+    <div class="fdfg"><img :src="banner" alt="banner" width="100%" /></div>
+    <div class="nft">
+      <div class="nft-left">
+        <div class="nft-left-top">
+          <img :src="nft2" alt="nft2" width="100%" />
+        </div>
+        <div class="nft-left-top">
+          <img :src="nft3" alt="nft3" width="100%" />
+        </div>
+        <div class="nft-left-top">
+          <img :src="nft1" alt="nft1" width="100%" />
+        </div>
+      </div>
+      <div class="nft-right"><img :src="p1" alt="p1" width="100%" /></div>
+    </div>
+    <div class="about">
+      <img :src="p2" alt="p2" width="100%" />
+    </div>
   </div>
 </template>
 
 <script>
 import banner from "@/assets/img/banner.png";
-console.log(11111111, banner);
+import nft1 from "@/assets/img/nft1.png";
+import nft2 from "@/assets/img/nft2.png";
+import nft3 from "@/assets/img/nft3.png";
+import p1 from "@/assets/img/p1.png";
+import p2 from "@/assets/img/p2.png";
+import logo from "@/assets/img/logo.png";
+import mint from "@/assets/img/mint.png";
+import h1 from "@/assets/img/h1.png";
 export default {
   name: "HelloWorld",
   data() {
@@ -17,6 +39,14 @@ export default {
       // banner: require("/src/assets/img/banner.png"),
       // active: require("@/assets/logo.png"),
       banner,
+      nft1,
+      nft2,
+      nft3,
+      p1,
+      p2,
+      logo,
+      mint,
+      h1,
     };
   },
   created() {
@@ -56,5 +86,19 @@ a {
   height: 100%;
   overflow: hidden;
   overflow-y: scroll;
+}
+.nft {
+  display: flex;
+  justify-content: space-between;
+}
+.nft-left {
+  width: 40%;
+}
+.nft-right {
+  width: 50%;
+}
+.nft-left-top {
+  width: 168px;
+  height: 168px;
 }
 </style>
